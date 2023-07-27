1f026a8ab88e9b32325f68c49de7ac4669a20f33
Yehor Dudnik <yehor.dudnik@gmail.com>
2023-07-27 01:10:35 +0100

    validation email, password

--- src/components/layout/Input.tsx
+++ src/components/layout/Input.tsx
@@ -1,22 +1,33 @@
-import React from 'react';
+import React, { ChangeEvent, FocusEvent } from "react";
+import { useField } from "formik";
 
 interface InputProps {
-    htmlFor: string;
-    text: string;
-    id: string;
-    type:string;
-    name:string;
-    accept:string;
-  }
+  htmlFor: string;
+  text: string;
+  id: string;
+  name: string;
+  type: string;
+  accept?: string;
+}
 
-function Input(props: InputProps){
-    return(
-        <div>
-          <label htmlFor={props.htmlFor}>{props.text}</label>
-          <input type={props.type} id={props.id} name={props.name} required accept={props.accept}/>
-        </div>
-    );
+const Input: React.FC<InputProps> = ({ htmlFor, text, id, name, type, accept }) => {
+  const [field, meta] = useField(name);
 
+  return (
+    <div>
+      <label htmlFor={htmlFor}>{text}</label>
+      <input
+        type={type}
+        id={id}
+        name={name}
+        required
+        accept={accept}
+        onChange={field.onChange}
+        onBlur={field.onBlur}
+        value={field.value}
+      />
+    </div>
+  );
 };
 
 export default Input;
