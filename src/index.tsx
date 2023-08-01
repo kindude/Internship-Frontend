f9be1606da47d451b653604d3e8c2336a0c511b6
Yehor Dudnik <yehor.dudnik@gmail.com>
2023-08-02 00:13:13 +0100

    finished

--- src/index.tsx
+++ src/index.tsx
@@ -4,17 +4,29 @@ import { Provider } from 'react-redux';
 import store  from './store/store';
 import AppRouter from './router';
 import { createRoot } from "react-dom/client";
-
+import { Auth0Provider } from '@auth0/auth0-react';
 
 const rootElement = document.getElementById('root');
+
+
 
 if (rootElement) {
   const root = createRoot(rootElement);
 
   root.render(
-    <Provider store={store}>
-      <AppRouter />
-    </Provider>
+    <Auth0Provider
+      domain={process.env.REACT_APP_DOMAIN || ""}
+      clientId={process.env.REACT_APP_CLIENT_ID || ""}
+      authorizationParams={{
+        redirect_uri: window.location.origin,
+        audience: "https://auth-reg",
+      }}
+      // Add any other Auth0 configurations as needed
+    >
+      <Provider store={store}>
+        <AppRouter />
+      </Provider>
+    </Auth0Provider>
   );
 } else {
   console.error("Element with id 'root' not found in the DOM.");
